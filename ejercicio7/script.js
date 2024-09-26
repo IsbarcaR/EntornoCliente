b44fa688d85f2df61f7beed012371977fbb37913
Isbarcar <ismael.barranco.2402@fernando3martos.com>
2024-09-26 08:54:48 +0200

    Ejercicios

---
+++ ejercicio7/script.js
@@ -0,0 +1,22 @@
+let frase1= "hola me llamo ismael";
+let frase2= frase1;
+
+console.log ("Apartado 1: Verificando si las frases son iguales");
+console.log(frase1==frase2);
+
+frase1="hola";
+
+console.log ("Apartado 2: Verificando si las frases son iguales");
+console.log(frase1==frase2);
+
+let num=2;
+console.log ("Apartado 3: expresión que involucre un string y un número y devuelva FALSE");
+console.log(frase1==num);
+
+console.log ("Apartado 4: expresión que involucre un string y un número y devuelva True");
+console.log(!(frase1==num));
+
+
+
+
+
